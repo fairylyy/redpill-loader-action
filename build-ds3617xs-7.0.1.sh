57bb875edfaaf8234b283488ce5b1eaf28c18be6
fairylyy <33444943+fairylyy@users.noreply.github.com>
2022-01-22 23:15:29 +0800

    Update build-ds3617xs-7.0.1.sh

--- build-ds3617xs-7.0.1.sh
+++ build-ds3617xs-7.0.1.sh
@@ -20,7 +20,7 @@ tar -C./broadwell/ -xf ds.broadwell-7.0.dev.txz usr/local/x86_64-pc-linux-gnu/x8
 
 # build redpill-lkm
 cd redpill-lkm
-sed -i 's/   -std=gnu89/   -std=gnu89 -fno-pie/' ../bromolow/usr/local/x86_64-pc-linux-gnu/x86_64-pc-linux-gnu/sys-root/usr/lib/modules/DSM-7.0/build/Makefile
+sed -i 's/   -std=gnu89/   -std=gnu89 -fno-pie/' ../broadwell/usr/local/x86_64-pc-linux-gnu/x86_64-pc-linux-gnu/sys-root/usr/lib/modules/DSM-7.0/build/Makefile
 make LINUX_SRC=../broadwell/usr/local/x86_64-pc-linux-gnu/x86_64-pc-linux-gnu/sys-root/usr/lib/modules/DSM-7.0/build dev-v7
 read -a KVERS <<< "$(sudo modinfo --field=vermagic redpill.ko)" && cp -fv redpill.ko ../redpill-load/ext/rp-lkm/redpill-linux-v${KVERS[0]}.ko || exit 1
 cd ..
